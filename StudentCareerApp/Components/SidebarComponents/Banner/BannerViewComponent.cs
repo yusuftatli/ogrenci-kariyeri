1248ba04d5150f8b4d1ae1071b6e58808af52716
goksudnz <goksudnz@gproject.ist>
2019-07-27 23:37:13 +0300

    banner component

---
+++ StudentCareerApp/Components/SidebarComponents/Banner/BannerViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentCareerApp.Components.SidebarComponents.Banner
+{
+    public class BannerViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string url, string imagePath)
+        {
+            ViewBag.Url = url;
+            ViewBag.ImagePath = imagePath;
+            return View("_Banner");
+        }
+    }
+}
