bad7af7fac945df84be6810bfa59e729421b0af6
goksudnz <goksudnz@gproject.ist>
2019-07-28 04:17:35 +0300

    company sayfası

---
+++ SCA.Entity/DTO/UserDto/EditorForUIDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCA.Entity.DTO
+{
+    public class WriterForUIDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public int PostCount { get; set; }
+        public int CommentCount { get; set; }
+        public List<SocialMediaDto> SocialMedias { get; set; }
+    }
+}
---
+++ StudentCareerApp/Components/EditorInformation/EditorInformationViewComponent.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SCA.Entity.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentCareerApp.Components.EditorInformation
+{
+    public class EditorInformationViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var model = FakeData();
+            return View("_EditorInformation", model);
+        }
+
+        public WriterForUIDto FakeData()
+        {
+            return new WriterForUIDto
+            {
+                Description = @"<p>
+                                1500'lerden beri kullanılan standart Lorem Ipsum pasajı
+                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                            </p>
+
+                            <p>
+                                M.Ö. 45 tarihinde Çiçero tarafından yazılan 'de Finibus Bonorum et Malorum'un 1.10.32 sayılı bölümü
+                                'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
+                            </p>",
+                Name = "Göksu Deniz",
+                ImagePath = "/images/news/user1.png",
+                PostCount = 22,
+                SocialMedias = new List<SocialMediaDto>
+                {
+                    new SocialMediaDto
+                    {
+                        FollowerCount = null,
+                        SocialMediaType = SCA.Entity.Enums.SocialMediaType.Facebook,
+                        Url = "https://www.facebook.com/goksu-deniz"
+                    },
+                    new SocialMediaDto
+                    {
+                        FollowerCount = null,
+                        SocialMediaType = SCA.Entity.Enums.SocialMediaType.Instagram,
+                        Url = "https://www.instagram.com/goksudnz"
+                    },
+                    new SocialMediaDto
+                    {
+                        FollowerCount = null,
+                        SocialMediaType = SCA.Entity.Enums.SocialMediaType.Linkedin,
+                        Url = "https://www.linkedin.com/goksudnz"
+                    },
+                    new SocialMediaDto
+                    {
+                        FollowerCount = null,
+                        SocialMediaType = SCA.Entity.Enums.SocialMediaType.Twitter,
+                        Url = "https://www.twitter.com/goksudnz"
+                    }
+                }
+            };
+        }
+    }
+}
