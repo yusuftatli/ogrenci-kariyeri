f14a1862f49f0687ed72b3b02eef01ceab74f6e7
goksudnz <goksudnz@gproject.ist>
2019-08-10 17:22:02 +0300

    youtube playlist component

---
+++ SCA.Entity/DTO/YoutubeVideo/YoutubeVideo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCA.Entity.DTO
+{
+    public class YoutubeVideo
+    {
+        public string VideoLink { get; set; }
+        public string ImagePath { get; set; }
+        public string Title { get; set; }
+    }
+
+    
+}
---
+++ StudentCareerApp/Components/YoutubePlaylist/YoutubePlaylistViewComponent.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SCA.Entity.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentCareerApp.Components.YoutubePlaylist
+{
+    public class YoutubePlaylistViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(List<YoutubeVideo> model = null)
+        {
+            var returnModel = model ?? FakeData();
+            return View("_YoutubePlaylist", returnModel);
+        }
+
+        public List<YoutubeVideo> FakeData()
+        {
+            return new List<YoutubeVideo>
+            {
+                new YoutubeVideo
+                {
+                    ImagePath = "/images/news/sports/sports4.jpg",
+                    Title = "Tourism in Dubai tourist favorite place",
+                    VideoLink = "https://www.youtube.com/watch?v=_0UO1NcheAE"
+                },
+                new YoutubeVideo
+                {
+                    ImagePath = "/images/news/sports/sports4.jpg",
+                    Title = "Tourism in Dubai tourist favorite place",
+                    VideoLink = "https://www.youtube.com/watch?v=_0UO1NcheAE"
+                },
+                new YoutubeVideo
+                {
+                    ImagePath = "/images/news/sports/sports4.jpg",
+                    Title = "Tourism in Dubai tourist favorite place",
+                    VideoLink = "https://www.youtube.com/watch?v=_0UO1NcheAE"
+                },
+                new YoutubeVideo
+                {
+                    ImagePath = "/images/news/sports/sports4.jpg",
+                    Title = "Tourism in Dubai tourist favorite place",
+                    VideoLink = "https://www.youtube.com/watch?v=_0UO1NcheAE"
+                },
+                new YoutubeVideo
+                {
+                    ImagePath = "/images/news/sports/sports4.jpg",
+                    Title = "Tourism in Dubai tourist favorite place",
+                    VideoLink = "https://www.youtube.com/watch?v=_0UO1NcheAE"
+                }
+            };
+        }
+    }
+}
