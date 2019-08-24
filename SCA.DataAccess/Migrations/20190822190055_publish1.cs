385c9638d060523739f68ddf15888226d5d36820
aspcan <yctatli@gmail.com>
2019-08-24 11:28:01 +0300

    commite

---
+++ SCA.DataAccess/Migrations/20190822190055_publish1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SCA.DataAccess.Migrations
+{
+    public partial class publish1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
