e12fd0de089e13051f2f647d70ec8aab505aacfa
aspcan <yctatli@gmail.com>
2019-09-08 09:26:00 +0300

    comment

--- SCA.Entity/DTO/Comments/CommentForUIDto.cs
+++ SCA.Entity/DTO/Comments/CommentForUIDto.cs
@@ -7,6 +7,9 @@ namespace SCA.Entity.DTO
    public class CommentForUIDto
     {
         public long Id { get; set; }
+        public long UserID { get; set; }
+        public long ArticleId { get; set; }
         public string Description { get; set; }
+
     }
 }
---
+++ SCA.Entity/DTO/Sync/UserSyncDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCA.Entity.DTO.Sync
+{
+    public class UserSyncDto
+    {
+        public string id { get; set; }
+        public string isim { get; set; }
+        public string mail { get; set; }
+        public string telefon { get; set; }
+        public string sifre { get; set; }
+        public string guvenlik { get; set; }
+        public string versiyon { get; set; }
+        public string dogum { get; set; }
+        public string cinsiyet { get; set; }
+        public string lise { get; set; }
+        public string sinif { get; set; }
+        public string universite { get; set; }
+        public string bolum { get; set; }
+        public string mezuniyet { get; set; }
+        public string il { get; set; }
+        public string kategoriler { get; set; }
+        public string tip { get; set; }
+        public string puan { get; set; }
+        public string ayPuan { get; set; }
+        public string referans { get; set; }
+        public string push { get; set; }
+        public string platform { get; set; }
+        public string sonGiris { get; set; }
+        public string tarih { get; set; }
+    }
+}
