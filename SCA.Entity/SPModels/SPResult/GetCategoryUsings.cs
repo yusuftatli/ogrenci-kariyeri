3be93082e898ffae736a2f65b10f62430078ad0d
goksudnz <goksudnz@gproject.ist>
2019-11-02 02:02:10 +0300

    popular categories

---
+++ SCA.Entity/SPModels/SPResult/GetCategoryUsings.cs
@@ -0,0 +1,11 @@
+﻿namespace SCA.Entity.SPModels.SPResult
+{
+    public class GetCategoryUsings
+    {
+        public long Id { get; set; }
+
+        public string Description { get; set; }
+
+        public int UsingCount { get; set; }
+    }
+}
