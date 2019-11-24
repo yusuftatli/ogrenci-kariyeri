92eb76e3a5e964e2b070110068336b67e60a270e
goksudnz <goksudnz@gproject.ist>
2019-11-24 13:35:18 +0300

    basic page admin tarafı tamamlandı.

---
+++ SCA.Entity/Enums/PageType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace SCA.Entity.Enums
+{
+    public enum PageType : byte
+    {
+        [Description("Header sayfası")]
+        HeaderPage = 1,
+
+        [Description("Footer sayfası")]
+        FooterPage = 2
+    }
+}
